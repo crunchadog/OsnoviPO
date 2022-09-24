f238af268bdeaeec42ebf1d599ec26f9a645cb9b
Kirill Bobrishov <bobryshov@niuitmo.ru>
2022-09-24 12:19:02 +0300

    first commit

--- main.c
+++ main.c
@@ -2,13 +2,15 @@
 #include <math.h>
 
 int main() {
-   //char str;
-   //float num;
+   char str[100];
+   float num;
 
-   //scanf("%s", &str);
-   //printf("String: %s \n", &str);
-   //scanf("%f", &num);
-   //printf("Number: %.2f", num);
+   printf("Enter string:");
+   gets(str);
+   printf("String: ", str);
+   puts(str);
+   scanf("%f", &num);
+   printf("Number: %.2f", num);
 
    //float a;
    //float z1;
@@ -17,9 +19,9 @@ int main() {
    //z1 = (sin(2 * a) + sin(5 * a) - sin(3 * a)) / (cos( a + 1) - 2 * pow(2, sin(2 * a)));
    //printf("z1: %.4f",z1);
 
-   int a;
-   float z2;
-   scanf("%d", &a);
-   z2 = 2*sin(a);
-   printf("2sin(%d = %.4f", a, z2);
+   //int a;
+   //float z2;
+   //scanf("%d", &a);
+   //z2 = 2*sin(a);
+   //printf("2sin(%d = %.4f", a, z2);
 }
