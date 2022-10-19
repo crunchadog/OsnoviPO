7dbee9f6824fcbe99f0ecb3af7495cab0b4e2033
Kirill <112944650+crunchadog@users.noreply.github.com>
2022-10-19 16:23:26 +0300

    Update main.c

--- lab3,4/4/main.c
+++ lab3,4/4/main.c
@@ -2,13 +2,14 @@
 
 int main (void) {
     int num, max_num, min_num;
-   scanf("%d", &num);
-   max_num = 1000 / num;
+    scanf("%d", &num);
+    printf("%d\n", ((num >= 1) && (num <= 1000)));
+    max_num = 1000 / num;
     min_num = num / 1;
-    printf("\n%d\n", max_num & min_num);
+    printf("%d", max_num & min_num);
 
-   int num2;
-   scanf("%d", &num2);
-   int bit = 1 & (num2 << 3);
-   printf("%d", bit);
+    int num2;
+    scanf("%d", &num2);
+    int bit = 1 & (num2 << 3);
+    printf("%d", bit);
 }
