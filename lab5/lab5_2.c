59568f0b6e3f5efb0835197adf6c559333b858a6
Kirill <112944650+crunchadog@users.noreply.github.com>
2022-11-16 16:05:50 +0300

    Update lab5_2.c

--- lab5/lab5_2.c
+++ lab5/lab5_2.c
@@ -1,35 +1,49 @@
-#include "stdio.h";
+#include <stdio.h>
 
-int main() {
-    int num;
-    scanf("%d", &num);
-    int A[num][num], B[num][num], C[num][num];
+int main()
+{
+    int matrix1, num1, matrix2, num2;
 
-    for (int i = 0; i < num; ++i) {
-        for (int j = 0; j < num; ++j) {
-            scanf("%d", &A[i][j]);
+    scanf("%d %d", &matrix1, &num1);
+    int matrix1_1[matrix1][num1];
+    for (int i = 0; i < matrix1; ++i) {
+        for (int j = 0; j < num1; ++j) {
+            scanf("%d", &matrix1_1[i][j]);
         }
     }
 
-    for (int i = 0; i < num; ++i) {
-        for (int j = 0; j < num; ++j) {
-            scanf("%d", &B[i][j]);
+    scanf("%d %d", &matrix2, &num2);
+    int matrix2_2[matrix2][num2];
+    for (int i = 0; i < matrix2; ++i) {
+        for (int j = 0; j < num2; ++j) {
+            scanf("%d", &matrix2_2[i][j]);
         }
     }
 
-    for (int i = 0; i < num; ++i) {
-        for (int j = 0; j < num; ++j) {
-            C[i][j] = 0;
-            for (int k = 0; k < num; ++k) {
-                C[i][j] += A[i][k] * B[k][j];
+    if (num1 == matrix2) {
+        int matrix3[matrix1][num2];
+        for (int i = 0; i < matrix1; ++i) {
+            for (int j = 0; j < num2; ++j) {
+                matrix3[i][j] = 0;
             }
         }
-    }
 
-    for (int i = 0; i < 2; ++i) {
-        for (int j = 0; j < 2; ++j) {
-            printf("%d ", C[i][j]);
+        for (int i = 0; i < num2; ++i) {
+            for (int j = 0; j < matrix1; ++j) {
+                for (int k = 0; k < num1; ++k) {
+                    matrix3[i][j] += matrix1_1[i][k] * matrix2_2[k][j];
+                }
+            }
         }
-        printf("\n");
-    }
+
+        for (int i = 0; i < matrix1; ++i) {
+            for (int j = 0; j < num2; ++j) {
+                printf("%d ", matrix3[i][j]);
+            }
+            printf("\n");
+        }
+    } else
+        printf("Error");
+
+    return 0;
 }
