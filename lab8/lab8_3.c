1559e64c3fbe60dedd11adcc6d2164b96d5afd24
Kirill <112944650+crunchadog@users.noreply.github.com>
2022-11-16 16:37:50 +0300

    Update lab8_3.c

--- lab8/lab8_3.c
+++ lab8/lab8_3.c
@@ -1,15 +1,31 @@
 #include "stdio.h"
 #include <string.h>
+#include <stdbool.h>
 
 int main() {
+    bool f = false;
     char str1[20];
     char str2[20];
     scanf("%s", str1);
     scanf("%s", str2);
 
-    if (strcmp(str1, str2) == 0) {
+    for (int i = 0; i < strlen(str1); ++i) {
+        if(str1[i] == str2[i]) {
+            f = true;
+        } else {
+            f = false;
+        }
+    }
+
+    if (f) {
         printf("String are equal");
     } else {
         printf("String aren`t equal");
     }
+
+//    if (strcmp(str1, str2) == 0) {
+//        printf("String are equal");
+//    } else {
+//        printf("String aren`t equal");
+//    }
 }
